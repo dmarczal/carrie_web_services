20459e97786673c0c85015e2e992f14061db9271
Diego Marczal <dmarczal@gmail.com>
2011-10-13 12:54:25 -0300

    change session store and fixed assests

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-CarrieWebServices::Application.config.session_store :cookie_store, :key => '_carrieWebServices_session'
+CarrieWebServices::Application.config.session_store :cookie_store, :key => '_carrie_web_services_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
